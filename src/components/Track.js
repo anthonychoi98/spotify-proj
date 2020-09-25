8816f7282c348c5e8489518c3125a3eb214c0200
Anthony Choi <froshiga@gmail.com>
2020-09-25 14:16:27 -0700

    scroll within track

--- src/components/Track.js
+++ src/components/Track.js
@@ -8,10 +8,10 @@ class Track extends React.Component{
         super(props);
   
     }
-    
+
     render(){
         return(
-            <div className="track" style={{display: 'flex', backgroundColor: "white", border: '1px solid black', borderRadius: '5px', margin:'5px', display: "flex"}}>
+            <div className="track" style={{display: 'flex', backgroundColor: "white", border: '1px solid black', borderRadius: '5px', margin:'5px', display: "flex", overflow: "auto"}}>
                 <h1 className="tracktitle" style={{height: 45, fontSize: 25}}>{this.props.title}</h1>
                 <p style={{fontSize: 15}}>by {this.props.artist}</p>
                 <Button variant="success" onClick={(e) => this.props.onClickCallback(e, this.props.uri)} style={{color: 'black', height:30, width: 45, marginLeft: "auto", marginRight: 8, marginTop: 8}}>></Button>
